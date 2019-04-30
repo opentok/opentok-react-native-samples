6da9831834984cf6db8df3da23194c7068f89137
msach22 <msach22@gmail.com>
2019-04-30 14:26:37 -0700

    update samples to use llatest react native and opentok react native versions

---
+++ BasicVideoChat/ios/OTInstall.swift
@@ -0,0 +1,9 @@
+//
+//  OTInstall.swift
+//  BasicVideoChat
+//
+//  Created by Manik Sachdeva on 4/30/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
--- Signaling/ios/Signaling-Bridging-Header.h
+++ Signaling/ios/Signaling-Bridging-Header.h
@@ -1,8 +1,4 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-#import <React/RCTBridgeModule.h>
-#import <React/RCTBridgeMethod.h>
-#import <React/RCTViewManager.h>
-#import <React/RCTEventEmitter.h>
-#import <OpenTok/OpenTok.h>
+
