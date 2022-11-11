48bf5a0b2af57cd5ebbf5a1cd344b0743861b7d1
Zachary Powell <zachary@zpweb.site>
2022-11-11 13:29:17 +0000

    Updated sample app to React native 0.70.3 (#117)
    * Updated to latest react native version
    
    * Removed typo
    
    * Updated to latest opentok react native package
    
    * Rebuilt basic video chat with latest react native version
    
    * Updated Signalling example to latest react native

--- BasicVideoChat/ios/OTInstall.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OTInstall.swift
-//  BasicVideoChat
-//
-//  Created by Manik Sachdeva on 4/30/19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
--- Signaling/ios/OTInstall.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OTInstall.swift
-//  Signaling
-//
-//  Created by Manik Sachdeva on 4/30/19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
