06a92c455b3cbd268c298301439655807fea281d
Jeff Swartz <jeff.swartz@vonage.com>
2023-10-23 16:00:56 -0700

    Fix linting issues

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { OTSession, OTPublisher, OTSubscriber } from 'opentok-react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
 
 class App extends Component {
   constructor(props) {
@@ -8,14 +9,23 @@ class App extends Component {
     this.apiKey = '';
     this.sessionId = '';
     this.token = '';
-   }
-   
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'column', paddingHorizontal: 100, paddingVertical: 50 }}>
-        <OTSession apiKey={this.apiKey} sessionId={this.sessionId} token={this.token}>
-          <OTPublisher style={{ width: 200, height: 200 }} />
-          <OTSubscriber style={{ width: 200, height: 200 }} />
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          paddingHorizontal: 100,
+          paddingVertical: 50,
+        }}>
+        <OTSession
+          apiKey={this.apiKey}
+          sessionId={this.sessionId}
+          token={this.token}>
+          <OTPublisher style={{width: 200, height: 200}} />
+          <OTSubscriber style={{width: 200, height: 200}} />
         </OTSession>
       </View>
     );
