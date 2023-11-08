9c6140936990def6508db1ea49330d4dd712030f
Jeff Swartz <jeff.swartz@vonage.com>
2023-11-08 13:40:31 -0800

    Multiparty sample

---
+++ Multiparty/App.js
@@ -0,0 +1,35 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.apiKey = '';
+    this.sessionId = '';
+    this.token = '';
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          paddingHorizontal: 100,
+          paddingVertical: 50,
+        }}>
+        <OTSession
+          apiKey={this.apiKey}
+          sessionId={this.sessionId}
+          token={this.token}>
+          <OTPublisher style={{width: 200, height: 200}} />
+          <OTSubscriber style={{width: 200, height: 200}} />
+        </OTSession>
+      </View>
+    );
+  }
+}
+
+export default App;
