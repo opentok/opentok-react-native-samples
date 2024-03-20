68519a710d26ce6fffbc89ad3a90b16b654b140a
Jeff Swartz <jeff.swartz@vonage.com>
2024-03-20 11:38:48 -0700

    Test PermissionsAndroid.PERMISSIONS.CAMERA

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, PermissionsAndroid} from 'react-native';
 import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
 
 class App extends Component {
@@ -11,6 +11,13 @@ class App extends Component {
       '1_MX40NzIwMzJ-fjE3MTA4ODU0ODAzNzJ-Uy9jMmtzS3VMWTRPNmlSTmhoZFFZby9Jfn5-';
     this.token =
       'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPTIxY2M5MGMyNTVmZDcyMmY2N2ZjMzM3ZWM0NTdlYzUxOGM4Zjc1ZjQ6c2Vzc2lvbl9pZD0xX01YNDBOekl3TXpKLWZqRTNNVEE0T0RVME9EQXpOekotVXk5ak1tdHpTM1ZNV1RSUE5tbFNUbWhvWkZGWmJ5OUpmbjUtJmNyZWF0ZV90aW1lPTE3MTA4ODU0ODAmbm9uY2U9MC42ODgzNDM4MDg2OTYwNDAzJnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MTM0Nzc0ODAmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
+    this.state = {
+      hasCameraPermission: true,
+    };
+    PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA).then(permission => {
+      this.setState({ hasCameraPermission: permission});
+      console.log('PermissionsAndroid.PERMISSIONS.CAMERA', permission);
+    })
   }
 
   render() {
@@ -22,7 +29,7 @@ class App extends Component {
           paddingHorizontal: 100,
           paddingVertical: 50,
         }}>
-        <Text>Test fix-screenshare-no-camera-permission</Text>
+        <Text>Test {String(this.state.hasCameraPermission)}</Text>
         <OTSession
           apiKey={this.apiKey}
           sessionId={this.sessionId}
