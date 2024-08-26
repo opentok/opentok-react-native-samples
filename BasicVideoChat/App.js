75421c7ae4d06941256f55b38cfcfdafa514d191
Jeff Swartz <jeff.swartz@vonage.com>
2024-08-26 00:12:04 -0700

    Test android-2.28.2-ios-2.28.1 in BasicVideoChat app

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -6,9 +6,11 @@ import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.apiKey = '';
-    this.sessionId = '';
-    this.token = '';
+    this.apiKey = '472032';
+    this.sessionId =
+      '2_MX40NzIwMzJ-fjE3MjM3NzExODA1ODJ-UE55c3gvQm5Wc2FSTnRTV1ZpQnNNWEZGfn5-';
+    this.token =
+      'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPThhYzA0YzE3MDNiYzhiZDYxM2JlOTkyYzNmM2IyYWMwNzc5MGZjMzc6c2Vzc2lvbl9pZD0yX01YNDBOekl3TXpKLWZqRTNNak0zTnpFeE9EQTFPREotVUU1NWMzZ3ZRbTVXYzJGU1RuUlRWMVpwUW5OTldFWkdmbjUtJmNyZWF0ZV90aW1lPTE3MjM3NzExODEmbm9uY2U9MC45NTYwMDQyMDM0MjI2MzI0JnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MjYzNjMxODEmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
   }
 
   render() {
