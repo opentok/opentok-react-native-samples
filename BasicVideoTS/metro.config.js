9207823a2fed02770867ef45269ed594d4c38670
Jeff Swartz <jeff.swartz@vonage.com>
2025-04-03 15:00:57 -0700

    Add TypeScript sample -- BasicVideoTS

---
+++ BasicVideoTS/metro.config.js
@@ -0,0 +1,21 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const path = require('path');
+
+const config = {
+  // Make Metro able to resolve required external dependencies
+  // watchFolders: [path.resolve(__dirname, '../Library')],
+  resolver: {
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
